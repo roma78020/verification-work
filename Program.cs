aab15fa9fca588ba1e56a28c21417146f9eed4e3
Roman Shiryaev <roma78020@yandex.ru>
2022-09-17 14:20:31 +0700

    Добавил метод для создания массива

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿string [] CreateStringArray (int size)
+{
+    string [] newArray = new string [size];
+    for (int i = 0; i < newArray.Length; i++)
+    {
+        Console.Write ($"Введите {i+1} значение массива");
+        newArray[i] = Console.ReadLine();
+    }
+    return newArray;
+}
+
