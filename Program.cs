9b6b4c3e6b6233413edbe14ecf869a306ddf9392
Roman Shiryaev <roma78020@yandex.ru>
2022-09-17 14:22:44 +0700

    Добавил метод для вывода  массива

--- Program.cs
+++ Program.cs
@@ -9,3 +9,11 @@
     return newArray;
 }
 
+void ShowArray (string [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
